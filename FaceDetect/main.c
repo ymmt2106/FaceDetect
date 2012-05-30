3524b5e85e1fdd6626043713953733488a05a383
ymmt <ymmt@g202-24-86-142.nakanishi.ac.jp>
2012-05-30 10:18:23 +0900

    Initial Commit

---
+++ FaceDetect/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FaceDetect
+//
+//  Created by 努武 山本 on 12/05/30.
+//  Copyright (c) 2012年 名古屋学芸大学. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
